66d504e46b37e42058242873885e4a3a6152b2ea
davidnjoroge <njorogedavid13@gmail.com>
2023-03-30 12:10:39 +0300

    add create field inputs

---
+++ src/views/CreateFieldView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>This is a create field</h1>
+    <div class="container text-left">
+
+      <CreateFieldForm/>
+
+      <v-divider/>
+      <FieldMappingSection />
+
+    </div>
+
+
+  </div>
+</template>
+<script setup>
+import CreateFieldForm from "@/views/CreateFieldForm.vue";
+import FieldMappingSection from "@/views/FieldMappingSection.vue";
+</script>
